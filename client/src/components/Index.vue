8f05065dbec4959c2f4579fff4fb6c7b1179994e
karnpapon <karnpapon@gmail.com>
2019-07-03 19:33:59 +0700

    first commit

---
+++ client/src/components/Index.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <h1 class="mt-5">SuperCollider development.</h1>
+            <p class="lead">frontend with Vue.js.</p>
+            <button 
+              @mousedown="send('noteOn',5.0,5.0)" 
+              @mouseup="send('noteOff', 5.0, 5.0)">
+                test audio
+            </button>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+const socket = io();
+export default {
+    name: 'Index',
+    methods: {
+      send(eventType, x, y) {
+        console.log(eventType, x, y);
+        socket.emit(eventType, {
+            x: 0.5,
+            y: 0.3
+        });
+      }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+    text-align: center;
+}
+</style>
---
+++ server/public/precache-manifest.81af435e1a4035ba61032966a56df25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "be63860db4481b4048bd",
+    "url": "/js/chunk-vendors.35e53164.js"
+  },
+  {
+    "revision": "a080fd1b45f1534e4875",
+    "url": "/js/app.fb674b93.js"
+  },
+  {
+    "revision": "e1380c365b1b33b56037766f4c2a34c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be63860db4481b4048bd",
+    "url": "/css/chunk-vendors.66440731.css"
+  },
+  {
+    "revision": "a080fd1b45f1534e4875",
+    "url": "/css/app.2ead7ca6.css"
+  }
+];
